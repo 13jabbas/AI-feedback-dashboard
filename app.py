3320da33f04588cf77a603102f4feaa4df94dea6
13jabbas <110024698+13jabbas@users.noreply.github.com>
2024-08-14 22:35:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import nltk
 # Load data
 @st.cache_data
 def load_data():
-    df = pd.read_csv('/path_to_data/LLMNER.csv')  # Modify path as needed
+    df = pd.read_csv('LLMNER.csv')  # Modify path as needed
     hallucinations = pd.read_csv('/path_to_data/Hallucination_Confidence_Score.csv')  # Modify path as needed
     return df, hallucinations
 
