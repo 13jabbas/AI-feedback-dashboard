7e6d4b8a6bd4f531ad1e8aa224a6d26106bf18da
13jabbas <110024698+13jabbas@users.noreply.github.com>
2024-08-18 17:30:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ st.header("Hallucinations Analysis")
 st.subheader("Word Cloud for Hallucinations")
 new_df = hallucinations[hallucinations['Keyword Match Count'] == 0]
 text = ' '.join(new_df['Description'].astype(str))
-wordcloud = WordCloud(width=800, height=400, background_color='black', colormap='white').generate(text)
+wordcloud = WordCloud(width=800, height=400, background_color='black', colormap='BuPu').generate(text)
 fig_wordcloud, ax = plt.subplots(figsize=(10, 5))
 ax.imshow(wordcloud, interpolation='bilinear')
 ax.axis('off')
