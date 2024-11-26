1647ff08f93e270343a3d1ca6e6fd6615e8d8292
13jabbas <110024698+13jabbas@users.noreply.github.com>
2024-11-26 18:27:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -308,7 +308,7 @@ from sklearn.metrics import roc_curve, roc_auc_score, f1_score
 import streamlit as st
 
 # Load the CSV data
-df = pd.read_csv('StreamlitRoc.csv')
+df = pd.read_csv('StreamlitRoc (1).csv')
 
 # Define ground truth and predicted probabilities
 y_true = df['hallucination_groundtruth']  # Ground truth (1 for hallucination, 0 for non-hallucination)
